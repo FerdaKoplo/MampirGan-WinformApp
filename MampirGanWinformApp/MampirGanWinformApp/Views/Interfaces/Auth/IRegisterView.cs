87f6ec6bd2f6505b8a52326ecc657f98f71e0d25
Rifqi Reissal Arasy <rifqiarasy@gmail.com>
2025-06-13 10:42:59 +0700

    progress interface Auth + add form dashboard customer

---
+++ MampirGanWinformApp/MampirGanWinformApp/Views/Interfaces/Auth/IRegisterView.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MampirGanWinformApp.Views.Interfaces.Auth
+{
+    interface IRegisterView
+    {
+        string Email { get; }
+        string Username { get; }
+        string Password { get; }
+        void NavigateToLogin();
+    }
+}
